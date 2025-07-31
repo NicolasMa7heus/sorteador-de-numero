48cc811de5a1d7c34db1f84f09424341afb02581
Nicolas Matheus <nicolasma7heus@gmail.com>
2025-07-31 11:53:26 -0300

    Criada a function sortear( ) que coleta os valores dos input e armazena nas variáveis.

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-// Nessa function sortear vamos recuperar o elemento pelo id e guardar o conteudo dele em uma variavel;
+// Criei essa função para que, ao clicar no botão Sortear, os valores digitados nos campos sejam coletados e armazenados dentro das variáveis.
 function sortear (){
     let quantidade = document.getElementById('quantidade').value; // Variavél criada para buscar o valor do input cuja o id="quantidade"
     let de = document.getElementById('de').value; // Variavél criada para buscar o valor do input cuja o id="de"
     let ate = document.getElementById('ate').value; // Variavél criada para buscar o valor do input cuja o id="ate"
 }
-//sincronização
