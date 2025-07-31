db2d6edbf2478689ca2c56c47a7b37dc8cb5261c
Nicolas Matheus <nicolasma7heus@gmail.com>
2025-07-31 11:23:04 -0300

    Criando a function sortar( ) que recupera o valor do input pelo id

---
+++ app.js
@@ -0,0 +1,6 @@
+// Nessa function sortear vamos recuperar o elemento pelo id e guardar o conteudo dele em uma variavel;
+function sortear (){
+    let quantidade = document.getElementById('quantidade').value; // Variavél criada para buscar o valor do input cuja o id="quantidade"
+    let de = document.getElementById('de').value; // Variavél criada para buscar o valor do input cuja o id="de"
+    let ate = document.getElementById('ate').value; // Variavél criada para buscar o valor do input cuja o id="ate"
+}
