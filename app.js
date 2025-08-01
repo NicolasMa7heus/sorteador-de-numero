6c8b7d0feed9a5b686c752dc8b8c6f093d23fa4c
Nicolas Matheus <nicolasma7heus@gmail.com>
2025-08-01 14:39:21 -0300

    Criada a função que gera o numero aleatorio, e criado um loop que devolve quantos numeros aleatórios foram selecionados no campo quantidade.

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 // Criei essa função para que, ao clicar no botão Sortear, os valores digitados nos campos sejam coletados e armazenados dentro das variáveis.
 function sortear (){
-    let quantidade = document.getElementById('quantidade').value; // Variavél criada para buscar o valor do input cuja o id="quantidade"
-    let de = document.getElementById('de').value; // Variavél criada para buscar o valor do input cuja o id="de"
-    let ate = document.getElementById('ate').value; // Variavél criada para buscar o valor do input cuja o id="ate"
+    let quantidade = parseInt(document.getElementById('quantidade').value); // Variavél criada para buscar o valor do input cuja o id="quantidade"
+    let de = parseInt(document.getElementById('de').value); 
+    let ate = parseInt(document.getElementById('ate').value);
+
+    let sorteados = [];
+    let numero;
+
+    // Criado uma variavel que vai ser o controlador do loop. E criamos um loop que vai de 0 até a *quantidade* que a pessoa digitou
+    for ( let i = 0; i < quantidade; i++){
+        numero = obterNumeroAleatorio (de, ate);// (de e até) seria o max e min da function 
+        sorteados.push(numero);
+    }
+    alert(sorteados);
+
+}
+/* Adicionado o parseInt para garantir que o valor que vai retornar pelo value seja do tipo numero inteiro,
+  porque por padrão o value retorna uma string */
+
+
+
+// Função para gerar o numero aleatorio entre o min e o max 
+function obterNumeroAleatorio(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min; // gera o numero aleatório entre os dois valaores inclusive o ultimo;
 }
